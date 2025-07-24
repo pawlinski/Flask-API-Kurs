999303ab73aa062d43c8d80a30f97cc16249a62a
Radek <radoslaw.pawlinski@gmail.com>
2025-07-24 17:46:30 +0200

    lesson 13 - request

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route('/') # strona główna aplikacji
 def index():
+    # print(request.headers)
+    # print(f'Method: {request.method}')
+    # print(f'Path: {request.path}')
+    # print(f'URL: {request.url}')
+    # print(request.headers['Authorization']) # drukujemy wartość nagłówka (klucza) Authorization
+    print(request.headers['Content-Type'])
+    print(request.json)
+    print(request.json['name'])
+    print(request.json.get('age'))
     return 'Hello, from Flask!!!!!!'
 
 if __name__ == '__main__':
